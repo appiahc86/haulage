ad6c39b0154f73f53ef589bab042b474ea844321
appiahc86 <appiahc86@gmail.com>
2020-12-25 06:11:32 +0000

    adding permissions

--- routes/home/index.js
+++ routes/home/index.js
@@ -6,8 +6,21 @@ import auth from "../../middleware/auth.js";
 //Welcome Page
 router.get('/', auth, async (req, res) => {
 
+   switch (parseInt(req.user.role)) {
 
-   res.render('home/index');
+      case 1 :
+         res.render('home/index');
+      break;
+
+      case 2 :
+         res.render('home/index');
+      break;
+
+      default:
+         res.render('home/user/index');
+   }
+
+
 
 });
 
--- routes/home/users.js
+++ routes/home/users.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 import userController from "../../controllers/userController/userController.js";
 import auth from "../../middleware/auth.js";
-
+import admin from "../../middleware/admin.js";
 //Get all users
-router.get('/', auth, userController.index);
+router.get('/', auth, admin, userController.index);
 
 //Registration form
-router.get('/register', auth, userController.registrationForm);
+router.get('/register', auth, admin, userController.registrationForm);
 
 //Register A User
-router.post('/register', auth, userController.register);
+router.post('/register', auth, admin, userController.register);
 
 //Login Form
 router.get('/login', userController.loginForm);
@@ -26,10 +26,10 @@ router.post('/password/reset', auth, userController.resetPassword);
 router.post('/login', userController.login);
 
 //Delete User
-router.delete('/delete/:id', auth, userController.destroy);
+router.delete('/delete/:id', auth, admin, userController.destroy);
 
 //Admin modify User
-router.patch('/admin/modify/:id', auth, userController.adminModify);
+router.patch('/admin/modify/:id', auth, admin, userController.adminModify);
 
 //Logout User
 router.get('/logout', auth, userController.logout);
